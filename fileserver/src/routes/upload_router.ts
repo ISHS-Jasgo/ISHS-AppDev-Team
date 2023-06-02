319e4e52ab42dcb44225ce389f1214081b5538a6
Jasgo <70329768+ISHS-Jasgo@users.noreply.github.com>
2023-06-02 15:50:56 +0900

    use logger

--- fileserver/src/routes/upload_router.ts
+++ fileserver/src/routes/upload_router.ts
@@ -1,3 +1,4 @@
+import { logger } from "../logging/central_log";
 import { respRest } from "../rest/rest_producer";
 import { FileUploadBuilder } from "../util/file_upload";
 import { Request, Response, NextFunction } from "express";
@@ -6,17 +7,17 @@ const uploadRouter = require('express').Router();
 const fileUploadBuilder = new FileUploadBuilder();
 
 uploadRouter.post('/image', fileUploadBuilder.setType("img").upload().single("file"), (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.file);
+    logger.info(req.file);
     res.status(200).send(respRest(200, "File uploaded"));
 });
 
 uploadRouter.post('/video', fileUploadBuilder.setType("video").upload().single("file"), (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.file);
+    logger.info(req.file);
     res.status(200).send(respRest(200, "File uploaded"));
 });
 
 uploadRouter.post('/audio', fileUploadBuilder.setType("audio").upload().single("file"), (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.file);
+    logger.info(req.file);
     res.status(200).send(respRest(200, "File uploaded"));
 });
 
