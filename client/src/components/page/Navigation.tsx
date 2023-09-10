0bd3c46426448b50c3bad9c28f5bacfe0216ecd8
0917ba <0917ba@naver.com>
2023-09-10 21:42:31 +0900

    design Navbar

---
+++ client/src/components/page/Navigation.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function Navigation() {
+  return (
+    <div className="bg-blue-500 md:basis-[2.5rem] basis-8 flex flex-col justify-center">
+      <div className="flex justify-center gap-10 text-white md:text-base text-sm">
+        <Link href="/info">학교 소개</Link>
+        <Link href="/knowledge">지식 in곽</Link>
+        <Link href="/calendar">학교 일정</Link>
+        <Link href="/learning">학습 자료</Link>
+        <Link href="/nostudy">면불 신청</Link>
+        <Link href="/club">동아리 리그전</Link>
+        <Link href="/bigbang">빅뱅</Link>
+      </div>
+    </div>
+  );
+}
